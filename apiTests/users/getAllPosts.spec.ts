d6d925771f7c37c1ab4a072444a6d2a777f044e0
kolesnikovak <katyamiller36@gmail.com>
2025-10-08 20:55:30 -0500

    getAllPosts api test created

---
+++ apiTests/users/getAllPosts.spec.ts
@@ -0,0 +1,26 @@
+import {test, expect} from '@playwright/test';
+import z from 'zod';
+
+const postSchema = z.object({
+    userId: z.number().int().positive().describe("ID of the user who created the post"),
+    id: z.number().int().positive().describe("Unique identifier for the post"),
+    title: z.string().min(1).max(200).describe("Title of the post"),
+    body: z.string().min(1).max(5000).describe("Content body of the post")
+});
+
+const baseUrl = process.env.BASE_URL;
+
+test('Get all posts API tests', async ({ request }) => {
+    const response = await request.get(`${baseUrl}/posts`);
+
+    const status = response.status();
+    const responseJSON = await response.json();
+
+    expect(status).toBe(200);
+
+    for (let i = 0; i < responseJSON.length; i++) {
+        postSchema.parse(responseJSON[i]);
+    }
+
+    console.log('Number of posts:', responseJSON.length);
+});
