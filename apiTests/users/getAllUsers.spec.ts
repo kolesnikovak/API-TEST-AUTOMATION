01231dcd16073edccc20c0e9c2a23923bf6a7521
kolesnikovak <katyamiller36@gmail.com>
2025-10-11 09:44:25 -0500

    Validated status code and created schema

--- apiTests/users/getAllUsers.spec.ts
+++ apiTests/users/getAllUsers.spec.ts
@@ -45,3 +45,61 @@ test('Get all users API tests', async ({ request }) => {
 
     console.log('Number of users:', responseJSON.length);
 })
+
+test('Validate status code and schema for single user', async ({ request }) => {
+    const userRequestBody = {
+        "id": 1,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874",
+            "geo": {
+                "lat": "-37.3159",
+                "lng": "81.1496"
+            }
+        },
+        "phone": "1-770-736-8031 x56442",
+        "website": "hildegard.org",
+        "company": {
+            "name": "Romaguera-Crona",
+            "catchPhrase": "Multi-layered client-server neural-net",
+            "bs": "harness real-time e-markets"
+        }
+    };
+
+    const createdUserResponse = await request.get(`${baseUrl}/users/${userRequestBody.id}`)
+    expect(createdUserResponse.status()).toBe(200)
+
+    const expectedResponseSchema = z.object({
+        id: z.literal(userRequestBody.id),
+        name: z.literal(userRequestBody.name),
+        username: z.literal(userRequestBody.username),
+        email: z.literal(userRequestBody.email),
+        address: z.object({
+            street: z.literal(userRequestBody.address.street),
+            suite: z.literal(userRequestBody.address.suite),
+            city: z.literal(userRequestBody.address.city),
+            zipcode: z.literal(userRequestBody.address.zipcode),
+            geo: z.object({
+                lat: z.literal(userRequestBody.address.geo.lat),
+                lng: z.literal(userRequestBody.address.geo.lng)
+            })
+        }),
+        phone: z.literal(userRequestBody.phone),
+        website: z.literal(userRequestBody.website),
+        company: z.object({
+            name: z.literal(userRequestBody.company.name),
+            catchPhrase: z.literal(userRequestBody.company.catchPhrase),
+            bs: z.literal(userRequestBody.company.bs)
+        })
+    });
+
+    const actualResponseJSON = await createdUserResponse.json()
+
+    expectedResponseSchema.parse(actualResponseJSON);
+    console.log('Single user details:', actualResponseJSON);
+});
